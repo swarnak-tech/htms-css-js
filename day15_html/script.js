21574fd74670c35ae039b64cd84bfa22eacac370
swarna-tech <swarnak99660@gmail.com>
2026-01-27 19:06:17 +0530

    day15 Initial Commit

---
+++ day15_html/script.js
@@ -0,0 +1,22 @@
+// {/* <script>
+// function getLocation() {
+//     const output = document.getElementById("output");
+
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition, showError);
+//     } else {
+//         output.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
+
+// function showPosition(position) {
+//     document.getElementById("output").innerHTML =
+//         "Latitude: " + position.coords.latitude +
+//         "<br>Longitude: " + position.coords.longitude;
+// }
+
+// function showError(error) {
+//     document.getElementById("output").innerHTML =
+//         "Permission denied or location unavailable.";
+// }
+// </script> */}
