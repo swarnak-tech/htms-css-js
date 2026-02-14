2ed2f68e839a0716346a3ca462e6270d26710af4
swarnak-tech <swarnak99660@gmail.com>
2026-02-14 11:18:53 +0530

    DAY-30-31

---
+++ Azure-D30-31/Demo_WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Demo_WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
